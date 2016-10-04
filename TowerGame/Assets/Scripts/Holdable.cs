49691d960bf6a935de52c1b606a05932cfab06ec
LBPeraza <lukasperaza@gmail.com>
2016-10-03 22:32:22 -0400

    create slingshot Prefab, Holdable script

---
+++ TowerGame/Assets/Scripts/Holdable.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Holdable : MonoBehaviour {
+	private GameObject holder;
+	private bool isHeld;
+
+	public float grabThreshold = 1.0f;
+
+	void Start () {
+		holder = null;
+		isHeld = false;
+		this.transform.tag = "Holdable";
+	}
+
+	public virtual void PickUp (GameObject hand) {
+		Transform handTransform = hand.transform;
+		if (!isHeld && Vector3.Distance(handTransform.position, transform.position) < grabThreshold) {
+			holder = hand;
+			isHeld = true;
+			this.transform.position = handTransform.position;
+			this.transform.rotation = handTransform.rotation;
+			this.transform.SetParent (holder.transform);
+		}
+	}
+
+	public void PutDown () {
+		this.transform.SetParent (null);
+		holder = null;
+		isHeld = false;
+	}
+
+	public bool IsHeld () {
+		return isHeld;
+	}
+}
---
+++ TowerGame/Assets/Scripts/Slingshot/Slingshot.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Slingshot : Holdable {
+	private GameObject slingHand;
+	private bool aiming = false;
+
+	public float aimThreshold = 1.0f;
+
+	public void PickUp (GameObject yokeHand, GameObject slingHand) {
+		base.PickUp (yokeHand);
+		this.slingHand = slingHand;
+	}
+
+	void StartShot () {
+		Transform slingTransform = slingHand.transform;
+		if (!aiming && Vector3.Distance (slingTransform.position, transform.position) < aimThreshold) {
+			aiming = true;
+		}
+	}
+}
