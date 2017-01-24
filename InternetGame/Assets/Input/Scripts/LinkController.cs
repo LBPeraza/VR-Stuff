afac99d208b10618cf1a97fdcf0179298317f213
Jon Buckley <jonbuckley33@gmail.com>
2017-01-23 22:00:32 -0500

    input manager is working 90% and link drawing is up

---
+++ InternetGame/Assets/Input/Scripts/LinkController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace InternetGame
+{
+    public class LinkController : MonoBehaviour
+    {
+        private GameObject CurrentLink;
+
+        // Use this for initialization
+        void Start()
+        {
+            InputManager.RightTriggerClicked += TriggerDown;
+            InputManager.RightTriggerUnclicked += TriggerUp;
+        }
+
+        public void TriggerDown(object sender, ClickedEventArgs args)
+        {
+            Debug.Log("Trigger down");
+            GameObject LinkContainer = LinkFactory.CreateLink();
+            LinkContainer.GetComponent<Link>().Initialize(InputManager.RightControllerObject.transform);
+
+            CurrentLink = LinkContainer;
+        }
+
+        public void TriggerUp(object sender, ClickedEventArgs args)
+        {
+            Debug.Log("Trigger up");
+            if (CurrentLink != null)
+            {
+                CurrentLink.GetComponent<Link>().End();
+                CurrentLink = null;
+            }
+        }
+    }
+}
+
