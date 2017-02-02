919e56c5b12eea8589b07f2a804ceab9e8595e87
Jon Buckley <jonbuckley33@gmail.com>
2017-02-02 12:10:50 -0500

    fixed hexagon math

--- InternetGame/Assets/Port/Scripts/Hexagon.cs
+++ InternetGame/Assets/Port/Scripts/Hexagon.cs
@@ -20,36 +20,49 @@ namespace InternetGame
             var rRoot3Over4 = radius * Mathf.Sqrt(3) / 4.0f;
             var threeFourthsR = 3.0f * radius / 4.0f;
             var rRoot3Over2 = radius * Mathf.Sqrt(3) / 2.0f;
-
-            // Left upper.
-            var leftUpper = Instantiate(HexagonSidePrefab, this.transform, false);
-            leftUpper.transform.localRotation = Quaternion.Euler(0, 0, -30.0f);
-            leftUpper.transform.localPosition = new Vector3(-threeFourthsR, rRoot3Over4, 0.0f);
-
-            // Left lower.
-            var leftLower = Instantiate(HexagonSidePrefab, this.transform, false);
-            leftLower.transform.localRotation = Quaternion.Euler(0, 0, 30.0f);
-            leftLower.transform.localPosition = new Vector3(-threeFourthsR, -rRoot3Over4, 0.0f);
+            var halfRingThickness = RingThickness / 2.0f;
 
             // Right upper.
             var rightUpper = Instantiate(HexagonSidePrefab, this.transform, false);
-            rightUpper.transform.localRotation = Quaternion.Euler(0, 0, 30.0f);
-            rightUpper.transform.localPosition = new Vector3(threeFourthsR, rRoot3Over4, 0.0f);
+            rightUpper.transform.localPosition = Vector3.zero;
+            rightUpper.transform.localPosition += new Vector3(halfRingThickness, 0);
+            rightUpper.transform.RotateAround(transform.parent.position, Vector3.forward, -30.0f);
+            rightUpper.transform.localPosition += new Vector3(-threeFourthsR, rRoot3Over4, 0.0f);
 
             // Right lower.
             var rightLower = Instantiate(HexagonSidePrefab, this.transform, false);
-            rightLower.transform.localRotation = Quaternion.Euler(0, 0, -30.0f);
-            rightLower.transform.localPosition = new Vector3(threeFourthsR, -rRoot3Over4, 0.0f);
+            rightLower.transform.localPosition = Vector3.zero;
+            rightLower.transform.localPosition += new Vector3(halfRingThickness, 0);
+            rightLower.transform.RotateAround(transform.parent.position, Vector3.forward, 30.0f);
+            rightLower.transform.localPosition += new Vector3(-threeFourthsR, -rRoot3Over4, 0.0f);
+
+            // Left upper.
+            var leftUpper = Instantiate(HexagonSidePrefab, this.transform, false);
+            leftUpper.transform.localPosition = Vector3.zero;
+            leftUpper.transform.localPosition += new Vector3(-halfRingThickness, 0);
+            leftUpper.transform.RotateAround(transform.parent.position, Vector3.forward, 30.0f);
+            leftUpper.transform.localPosition += new Vector3(threeFourthsR, rRoot3Over4, 0.0f);
+
+            // Left lower.
+            var leftLower = Instantiate(HexagonSidePrefab, this.transform, false);
+            leftLower.transform.localPosition = Vector3.zero;
+            leftLower.transform.localPosition += new Vector3(-halfRingThickness, 0);
+            leftLower.transform.RotateAround(transform.parent.position, Vector3.forward, -30.0f);
+            leftLower.transform.localPosition += new Vector3(threeFourthsR, -rRoot3Over4, 0.0f);
 
             // Top.
             var top = Instantiate(HexagonSidePrefab, this.transform, false);
-            top.transform.localRotation = Quaternion.Euler(0, 0, 90.0f);
-            top.transform.localPosition = new Vector3(0, rRoot3Over2, 0);
+            top.transform.localPosition = Vector3.zero;
+            top.transform.localPosition += new Vector3(-halfRingThickness, 0);
+            top.transform.RotateAround(transform.parent.position, Vector3.forward, 90.0f);
+            top.transform.localPosition += new Vector3(0, rRoot3Over2, 0);
 
             // Bottom.
             var bottom = Instantiate(HexagonSidePrefab, this.transform, false);
-            bottom.transform.localRotation = Quaternion.Euler(0, 0, 90.0f);
-            bottom.transform.localPosition = new Vector3(0, -rRoot3Over2, 0);
+            bottom.transform.localPosition = Vector3.zero;
+            bottom.transform.localPosition += new Vector3(halfRingThickness, 0);
+            bottom.transform.RotateAround(transform.parent.position, Vector3.forward, 90.0f);
+            bottom.transform.localPosition += new Vector3(0, -rRoot3Over2, 0);
 
             foreach (Transform t in this.transform)
             {
