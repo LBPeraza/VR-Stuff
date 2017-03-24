8d46a80412e15e53a35ae1fbdfd7c74df4177f7d
LBPeraza <lukasperaza@gmail.com>
2017-03-24 15:49:13 -0400

    import keyboard, modify for us

---
+++ InternetGame/Assets/Handcrafted Mobile/CurvedVRKeyboard/Resources/Scripts/KeyboardComponent.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+namespace CurvedVRKeyboard {
+
+    /// <summary>
+    /// Setup class derived by all classes who are part of keyboard,
+    /// so those variables are easy accessable everywhere
+    /// </summary>
+    public abstract class KeyboardComponent: MonoBehaviour {
+
+        // Special signs. Feel free to change
+        public const string SPACE = "  ";
+        public const string BACK = "Back";
+
+        public const int CENTER_ITEM = 15;
+        public const int KEY_NUMBER = 27;
+        public const int POSITION_SPACE = 28;
+
+        public enum KeyLetterEnum {
+            UpperCase
+        }
+
+        // Feel free to change (but do not write strings in place of
+        // special signs, change variables values instead)
+        // Remember to always have 30 values
+        public static readonly string[] allLettersUppercase = new string[]
+        {
+        "Q","W","E","R","T","Y","U","I","O","P",
+        "A","S","D","F","G","H","J","K","L",
+        BACK,"Z","X","C","V","B","N","M"
+        };
+
+        // Number of items in a row
+        public static readonly int[] lettersInRowsCount = new int[] { 10, 9, 8 };
+
+        /// <summary>
+        /// Checks for errrors with array of keys. 
+        /// </summary>
+        public static void CheckKeyArrays () {
+            if(allLettersUppercase.Length != KEY_NUMBER) {
+                ErrorReporter.Instance.SetMessage("There is incorrect amount of letters in Uppercase array. Check KeyboardComponent class", ErrorReporter.Status.Error);
+                return;
+            }
+        }
+    }
+}
---
+++ InternetGame/Assets/Handcrafted Mobile/CurvedVRKeyboard/Resources/Scripts/KeyboardStatus.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace CurvedVRKeyboard {
+    [SelectionBase]
+    public class KeyboardStatus: KeyboardComponent {
+
+        //------SET IN UNITY-------
+        [Tooltip("Text field receiving input from the keyboard")]
+        public Text output;
+        [Tooltip("Maximum output text length")]
+        public int maxOutputLength;
+
+        //----CurrentKeysStatus----
+        private KeyboardItem[] keys;
+        private bool areLettersActive = true;
+        private bool isLowercase = true;
+        private static readonly char BLANKSPACE = ' ';
+
+        public delegate void OnSubmittedName(string name);
+        public delegate void OnSkippedEntry();
+
+        public event OnSubmittedName SubmittedName;
+        public event OnSkippedEntry SkippedEntry;
+
+
+        /// <summary>
+        /// Handles click on keyboarditem
+        /// </summary>
+        /// <param name="clicked">keyboard item clicked</param>
+        public void HandleClick ( KeyboardItem clicked ) {
+            string value = clicked.GetValue();
+            if(value.Equals(SPACE)) {
+                TypeKey(BLANKSPACE);
+            } else if(value.Equals(BACK)) {
+                BackspaceKey();
+            } else {// Normal letter
+                TypeKey(value[0]);
+            }
+        }
+
+        private void BackspaceKey () {
+            if(output.text.Length >= 1)
+                output.text = output.text.Remove(output.text.Length - 1, 1);
+        }
+
+        private void TypeKey ( char key ) {
+            if(output.text.Length < maxOutputLength)
+                output.text = output.text + key.ToString();
+        }
+
+        public void SetKeys ( KeyboardItem[] keys ) {
+            this.keys = keys;
+        }
+    }
+}
