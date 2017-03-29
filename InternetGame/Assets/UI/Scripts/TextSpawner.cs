fff9960c3ff99652b211094843a3abfaf96cdf8d
LBPeraza <lukasperaza@gmail.com>
2017-03-29 18:53:53 -0400

    start PopupTextSpawner

---
+++ InternetGame/Assets/UI/Scripts/TextSpawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextSpawner : MonoBehaviour {
+
+	public static GameObject TextCanvasPrefab;
+	public GameObject TextCanvas;
+
+	void Start() {
+		Initialize ();
+	}
+
+	public void Initialize() {
+		if (TextCanvasPrefab == null)
+			TextCanvasPrefab = Resources.Load<GameObject> ("Prefabs/PopupTextCanvas");
+
+		if (TextCanvas == null) {
+			TextCanvas = Instantiate (TextCanvasPrefab, transform);
+			TextCanvas.transform.localPosition = Vector3.zero;
+		}
+	}
+
+	void Update() {
+		if (TextCanvas) {
+			Vector3 v = Camera.main.transform.position - TextCanvas.transform.position;
+			v.x = v.z = 0.0f;
+			TextCanvas.transform.LookAt (Camera.main.transform.position - v);
+			TextCanvas.transform.Rotate (0, 180, 0);
+		}
+	}
+}
